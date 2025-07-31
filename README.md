a21ada12de27320666b3af19a3a8421cc0419f62
Timur Gafarov <gecko0307@gmail.com>
2025-07-31 21:44:50 +0300

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bindbc-vulkan
-Dynamic binding to Vulkan
+# BindBC-Vulkan
+
+Dynamic binding to Vulkan graphics and compute API.
---
+++ src/bindbc/vulkan/base.d
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2015-2016 The Khronos Group Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and/or associated documentation files (the
+ * "Materials"), to deal in the Materials without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Materials, and to
+ * permit persons to whom the Materials are furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Materials.
+ *
+ * THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+ */
+module bindbc.vulkan.base;
+
+extern(System):
+
+alias VK_VERSION_MAJOR = (uint ver) => ver >> 22;
+alias VK_VERSION_MINOR = (uint ver) => (ver >> 12) & 0x3ff;
+alias VK_VERSION_PATCH = (uint ver) => ver & 0xfff;
+alias VK_MAKE_VERSION  = (uint major, uint minor, uint patch) =>
+    (major << 22) | (minor << 12) | patch;
+
+package mixin template VK_DEFINE_HANDLE(string name)
+{
+    mixin("struct " ~ name ~ "_T; \n alias " ~ name ~ " = " ~ name ~ "_T*;");
+}
+
+package mixin template VK_DEFINE_NON_DISPATCHABLE_HANDLE(string name)
+{
+    mixin("alias " ~ name ~ " = void*;");
+}
+
+alias VkFlags      = uint;
+alias VkBool32     = uint;
+alias VkDeviceSize = ulong;
+alias VkSampleMask = uint;
+
+enum VK_VERSION_1_0 = 1;
+enum VK_API_VERSION = VK_MAKE_VERSION(1, 0, 3); // Vulkan API version supported by this binding
+enum VK_NULL_HANDLE = null;
+
+enum VK_TRUE  = 1;
+enum VK_FALSE = 0;
+
+enum VK_UUID_SIZE  = 16;
+enum VK_WHOLE_SIZE = (~0UL);
+
+enum VK_LOD_CLAMP_NONE    = 1000.0f;
+enum VK_ATTACHMENT_UNUSED = (~0U);
+enum VK_SUBPASS_EXTERNAL  = (~0U);
+
+enum VK_QUEUE_FAMILY_IGNORED   = (~0U);
+enum VK_REMAINING_MIP_LEVELS   = (~0U);
+enum VK_REMAINING_ARRAY_LAYERS = (~0U);
+
+enum VK_MAX_MEMORY_TYPES        = 32;
+enum VK_MAX_MEMORY_HEAPS        = 16;
+enum VK_MAX_EXTENSION_NAME_SIZE = 256;
+enum VK_MAX_DESCRIPTION_SIZE    = 256;
+enum VK_MAX_PHYSICAL_DEVICE_NAME_SIZE = 256;
